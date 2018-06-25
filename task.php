6c74121e560186165e5fec0faddbda385e54582d
DmitriyBaran <33020093+DmitriyBaran@users.noreply.github.com>
2018-06-25 15:26:43 +0300

    Update task.php

--- task.php
+++ task.php
@@ -17,7 +17,7 @@ while ($row = mysqli_fetch_assoc($result)){
         $tmp_position = array_search($row['ident'],$_GET['ident']);
         if($row['version'] > $_GET['version'][$tmp_position])
         {
-            $update[$row['ident']] = array();
+            $update[$row['ident']] = array('value'= $row['version']);
         }
         unset($_GET['ident'][$tmp_position]);
         unset($_GET['value'][$tmp_position]);
